0d066840bb76002bbb005db13a52d5e55ff1b791
cheruiyotkelvin <ronohkelvin99@gmail.com>
2021-04-17 12:53:10 +0300

    Add media queries to the css

--- app.js
+++ app.js
@@ -1,6 +1,16 @@
 $(document).ready(function(){
 
 
+    $(".hover").mousemove(function(e){
+
+        var hovertext = $(this).attr('hovertext');
+        $("#hoverdiv").text(hovertext).show();
+        $("#hovertext").css('top', e.clientY+10).css('left', e.clientX+10);
+
+    }).mouseout(function(){
+        $("#hoverdiv").hide();
+    })
+
     $(".development_img").click(function(){
         $(".devText").show();
         $(this).hide()
@@ -29,4 +39,23 @@ $(document).ready(function(){
         $(".product_img").show();
         $(this).hide()
     })
+
+
+    $("#button").click(function(e){
+        e.preventDefault();
+        var email =$("#email").val();
+        var textarea =$("#textarea").val()
+        var name =$("#name").val()
+
+        if(name===""|| textarea===""|| email===""){
+            alert("Please fill all the fields")
+            return;
+        }
+        else {
+        
+         alert(name+", Email from "+email+ " has been received. Thank you")
+       
+        }
+    })
+
 })
